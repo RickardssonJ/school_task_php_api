86c70662a4c6a8f5758a2b43f7b51420901782f2
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2021-04-28 22:18:37 +0200

    removed code

--- index.php
+++ index.php
@@ -2,7 +2,4 @@
 $pokemons = file_get_contents("/Applications/MAMP/htdocs/myAPI/products.json");
 $json = json_decode($pokemons);
 
-echo "<pre>";
 echo $pokemons;
-//print_r($pokemons);
-echo "</pre>";
