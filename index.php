e24d69b944b682f60be4d5af6aa4ad7ce2bc3dd5
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2021-04-30 20:49:29 +0200

    Change from JSON to php array

--- index.php
+++ index.php
@@ -2,4 +2,19 @@
 $pokemons = file_get_contents("./products.json");
 $json = json_decode($pokemons);
 
-echo $pokemons;
+
+//echo $pokemons;
+
+if ($_GET['show']) {
+    $show = $_GET['show'];
+    $renderPokemonsArray = [];
+
+    for ($i = 0; $i < $show; $i++) {
+
+        $renderPokemons = $json[rand(0, 19)];
+        array_push($renderPokemonsArray, $renderPokemons);
+    }
+    echo "<pre>";
+    print_r($renderPokemonsArray);
+    echo "</pre>";
+}
