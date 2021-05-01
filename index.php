eaf0bd0f442b64fccf7dd209d6e9530583b8f059
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2021-05-01 17:13:59 +0200

    Working code

--- index.php
+++ index.php
@@ -1,20 +1,50 @@
 <?php
-$pokemons = file_get_contents("./products.json");
-$json = json_decode($pokemons);
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Referrer-Policy: no-referrer");
+
+require_once 'products.php';
+
+//skapa en array utan dubletter med ett max antal av 5
+
+$limit = isset($_GET['show']) ? $_GET['show'] : 19;
+$category = isset($_GET['category']) ? $_GET['category'] : false;
+
+$pokemons = array();
 
 
-//echo $pokemons;
+if ($limit) {
+    for ($i = 0; $i < $limit; $i++) {
 
-if ($_GET['show']) {
-    $show = $_GET['show'];
-    $renderPokemonsArray = [];
-
-    for ($i = 0; $i < $show; $i++) {
-
-        $renderPokemons = $json[rand(0, 19)];
-        array_push($renderPokemonsArray, $renderPokemons);
+        $numberOfPokemons = $pokemonData[rand(0, 19)];
+        array_push($pokemons, $numberOfPokemons);
     }
-    echo "<pre>";
-    print_r($renderPokemonsArray);
-    echo "</pre>";
 }
+
+
+if ($category) {
+    $pokemonsCategory = array();
+    $pokemonsLimitedCategory = array();
+
+    if ($limit > 5) {
+        $limit = 5;
+    } else {
+        $limit = $limit;
+    }
+
+    foreach ($pokemonData as $key => $value) {
+        if ($value['category'] == $category) {
+            array_push($pokemonsCategory, $value);
+        }
+    }
+    for ($i = 0; $i < $limit; $i++) {
+        array_push($pokemonsLimitedCategory, $pokemonsCategory[rand(0, $limit)]);
+    }
+    $pokemons = $pokemonsLimitedCategory;
+}
+
+$json = json_encode($pokemons, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+
+echo $json;
--- products.php
+++ products.php
@@ -2,140 +2,140 @@
 
 $pokemonData = array(
   array(
-    "id" => "1",
+    "id" => 1,
     "name" => "bulbasaur",
     "category" => "grass",
     "description" => "bulbasaur is a Grass/Poison type Pokémon introduced in Generation 1. It is known as the Seed Pokémon.",
     "img" => "https://img.pokemondb.net/artwork/large/bulbasaur.jpg"
   ),
   array(
-    "id" => "2",
+    "id" => 2,
     "name" => "oddish",
     "category" => "grass",
     "description" => "Oddish is a Grass/Poison type Pokémon introduced in Generation 1. It is known as the Weed Pokémon.",
     "img" => "https://img.pokemondb.net/artwork/large/oddish.jpg"
   ),
   array(
-    "id" => "3",
+    "id" => 3,
     "name" => "victreebel",
     "category" => "grass",
     "description" => "Victreebel is a Grass/Poison type Pokémon introduced in Generation 1. It is known as the Flycatcher Pokémon.",
     "img" => "https://img.pokemondb.net/artwork/large/victreebel.jpg"
   ),
   array(
-    "id" => "4",
-    "name" => "exeggutor",
+    "id"  => 4,
+    "name"  => "exeggutor",
     "category" => "grass",
     "description" => "Exeggutor is a Grass/Psychic type Pokémon introduced in Generation 1. It is known as the Coconut Pokémon.",
     "img" => "https://img.pokemondb.net/artwork/large/exeggutor.jpg"
   ),
   array(
-    "id" => "5",
+    "id" => 5,
     "name" => "meganium",
     "category" => "grass",
     "description" => "Meganium is a Grass type Pokémon introduced in Generation 2. It is known as the Herb Pokémon.",
     "img" => "https://img.pokemondb.net/artwork/large/meganium.jpg"
   ),
   array(
-    "id" => "6",
+    "id" => 6,
     "name" => "pikachu",
     "category" => "electric",
     "description" => "Pikachu is an Electric type Pokémon introduced in Generation 1. It is known as the Mouse Pokémon.",
     "img" => "https://img.pokemondb.net/artwork/pikachu.jpg"
   ),
   array(
-    "id" => "7",
+    "id" => 7,
     "name" => "raichu",
     "category" => "electric",
     "description" => "Raichu is an Electric type Pokémon introduced in Generation 1. It is known as the Mouse Pokémon.",
     "img" => "https://img.pokemondb.net/artwork/raichu.jpg"
   ),
   array(
-    "id" => "8",
+    "id" => 8,
     "name" => "electrode",
     "category" => "electric",
     "description" => "Electrode is an Electric type Pokémon introduced in Generation 1. It is known as the Ball Pokémon.",
     "img" => "https://img.pokemondb.net/artwork/electrode.jpg"
   ),
   array(
-    "id" => "9",
+    "id" => 9,
     "name" => "electabuzz",
     "category" => "electric",
     "description" => "Electabuzz is an Electric type Pokémon introduced in Generation 1. It is known as the Electric Pokémon.",
     "img" => "https://img.pokemondb.net/artwork/electabuzz.jpg"
   ),
   array(
-    "id" => "10",
+    "id" => 10,
     "name" => "zapdos",
     "category" => "electric",
     "description" => "Zapdos is an Electric/Flying type Pokémon introduced in Generation 1. It is known as the Electric Pokémon.",
     "img" => "https://img.pokemondb.net/artwork/zapdos.jpg"
   ),
   array(
-    "id" => "11",
+    "id" => 11,
     "name" => "charizard",
     "category" => "fire",
     "description" => "Charizard is a Fire/Flying type Pokémon introduced in Generation 1. It is known as the Flame Pokémon. Charizard has two Mega Evolutions, available from X & Y onwards.",
     "img" => "https://img.pokemondb.net/artwork/large/charizard.jpg"
   ),
   array(
-    "id" => "12",
+    "id" => 12,
     "name" => "vulpix",
     "category" => "fire",
     "description" => "Vulpix is a Fire type Pokémon introduced in Generation 1. It is known as the Fox Pokémon.Vulpix has a new Alolan form introduced in Pokémon Sun/Moon.",
     "img" => "https://img.pokemondb.net/artwork/vulpix.jpg"
   ),
   array(
-    "id" => "13",
+    "id" => 13,
     "name" => "rapidash",
     "category" => "fire",
     "description" => "Rapidash is a Fire type Pokémon introduced in Generation 1. It is known as the Fire Horse Pokémon.Rapidash has a new Galarian form in Pokémon Sword & Shield.",
     "img" => "https://img.pokemondb.net/artwork/rapidash.jpg"
   ),
   array(
-    "id" => "14",
+    "id" => 14,
     "name" => "magmar",
     "category" => "fire",
     "description" => "Magmar is a Fire type Pokémon introduced in Generation 1. It is known as the Spitfire Pokémon.",
     "img" => "https://img.pokemondb.net/artwork/magmar.jpg"
   ),
   array(
-    "id" => "15",
+    "id" => 15,
     "name" => "moltres",
     "category" => "fire",
     "description" => "Moltres is a Fire/Flying type Pokémon introduced in Generation 1. It is known as the Flame Pokémon.",
     "img" => "https://img.pokemondb.net/artwork/moltres.jpg"
   ),
   array(
-    "id" => "16",
+    "id" => 16,
     "name" => "blastoise",
     "category" => "water",
     "description" => "Blastoise is a Water type Pokémon introduced in Generation 1. It is known as the Shellfish Pokémon. Blastoise has a Mega Evolution, available from X & Y onwards.",
     "img" => "https://img.pokemondb.net/artwork/large/blastoise.jpg"
   ),
   array(
-    "id" => "17",
+    "id" => 17,
     "name" => "seadra",
     "category" => "water",
     "description" => "Seadra is a Water type Pokémon introduced in Generation 1. It is known as the Dragon Pokémon.",
     "img" => "https://img.pokemondb.net/artwork/seadra.jpg"
   ),
   array(
-    "id" => "18",
+    "id" => 18,
     "name" => "dewgong",
     "category" => "water",
     "description" => "Dewgong is a Water/Ice type Pokémon introduced in Generation 1. It is known as the Sea Lion Pokémon.",
     "img" => "https://img.pokemondb.net/artwork/dewgong.jpg"
   ),
   array(
-    "id" => "19",
+    "id" => 19,
     "name" => "poliwrath",
     "category" => "water",
     "description" => "Poliwrath is a Water/Fighting type Pokémon introduced in Generation 1. It is known as the Tadpole Pokémon.",
     "img" => "https://img.pokemondb.net/artwork/poliwrath.jpg"
   ),
   array(
-    "id" => "20",
+    "id" => 20,
     "name" => "golduck",
     "category" => "water",
     "description" => "Golduck is a Water type Pokémon introduced in Generation 1. It is known as the Duck Pokémon.",
